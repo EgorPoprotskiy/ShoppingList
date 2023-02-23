0e0542ee5be564af356630a2f7675460535a348f
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-02-23 15:11:02 +1000

    create_PresentationLayer

--- app/src/main/java/com/egorpoprotskiy/shoppinglist/Data/ShoppingRepositoryImpl.kt
+++ app/src/main/java/com/egorpoprotskiy/shoppinglist/Data/ShoppingRepositoryImpl.kt
@@ -5,36 +5,43 @@ import com.egorpoprotskiy.shoppinglist.Domain.ShoppingRepository
 //2 Добавление Data-слоя
 object ShoppingRepositoryImpl: ShoppingRepository {
 
-    private val ListAllShopping = mutableListOf<ListShopping>()
+    private val listAllShopping = mutableListOf<ListShopping>()
 
     private var autoIncrementId = 0
 
+    init {
+        for (i in 1 until 10) {
+            val item = ListShopping("name $i", i, true)
+            addItemShopping(item)
+        }
+    }
+
     override fun addItemShopping(listShopping: ListShopping) {
-        //проверка, добавляется новый элемент или добавляется существующий элемент при редактированит.
+        //проверка, добавляется новый элемент или добавляется существующий элеме нт при редактированит.
         if (listShopping.id == ListShopping.ID_NOTFOUND) {
             listShopping.id = autoIncrementId++
         }
-        ListAllShopping.add(listShopping)
+        listAllShopping.add(listShopping)
     }
 
     override fun deleteItemShopping(listShopping: ListShopping) {
-        ListAllShopping.remove(listShopping)
+        listAllShopping.remove(listShopping)
     }
 
     override fun editItemShopping(listShopping: ListShopping) {
         //при редактировании элемента, надо сначала его удалить и затем добавить новый(измененный)
         val oldItemId = getItemShopping(listShopping.id)
-        ListAllShopping.remove(oldItemId)
-        ListAllShopping.add(listShopping)
+        listAllShopping.remove(oldItemId)
+        listAllShopping.add(listShopping)
     }
 
     override fun getItemShopping(listShoppingId: Int): ListShopping {
         //проверка на null, если элемент с нужным id не найден, то будет исключение
-        return ListAllShopping.find { it.id == listShoppingId} ?: throw java.lang.RuntimeException("Element with ID $listShoppingId not found")
+        return listAllShopping.find { it.id == listShoppingId} ?: throw java.lang.RuntimeException("Element with ID $listShoppingId not found")
     }
 
     override fun getListShopping(): List<ListShopping> {
         //возвращать необходимо копию списка
-        return ListAllShopping.toList()
+        return listAllShopping.toList()
     }
 }
--- app/src/main/java/com/egorpoprotskiy/shoppinglist/Domain/ListShopping.kt
+++ app/src/main/java/com/egorpoprotskiy/shoppinglist/Domain/ListShopping.kt
@@ -2,7 +2,7 @@ package com.egorpoprotskiy.shoppinglist.Domain
 
 //1 Добавление Domain-слоя
 
-class ListShopping(val name: String, val count: Int, val value: Boolean, var id: Int = ID_NOTFOUND) {
+data class ListShopping(val name: String, val count: Int, val value: Boolean, var id: Int = ID_NOTFOUND) {
 
     companion object {
         const val ID_NOTFOUND = -1

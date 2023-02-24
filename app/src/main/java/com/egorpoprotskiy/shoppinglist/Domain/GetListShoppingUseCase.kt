e30c6903e0ee431e00231cb92acd9edc91a6e65e
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-02-25 00:51:41 +1000

    auto-updating list

--- app/src/main/java/com/egorpoprotskiy/shoppinglist/Domain/GetListShoppingUseCase.kt
+++ app/src/main/java/com/egorpoprotskiy/shoppinglist/Domain/GetListShoppingUseCase.kt
@@ -1,8 +1,10 @@
 package com.egorpoprotskiy.shoppinglist.Domain
 
+import androidx.lifecycle.LiveData
+
 //1 Добавление Domain-слоя
 class GetListShoppingUseCase(private val shoppingRepository: ShoppingRepository) {
-    fun getListShopping(): List<ListShopping> {
+    fun getListShopping(): LiveData<List<ListShopping>> {
         return shoppingRepository.getListShopping()
     }
 }
--- app/src/main/java/com/egorpoprotskiy/shoppinglist/Presentation/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/shoppinglist/Presentation/MainActivity.kt
@@ -9,14 +9,21 @@ import com.egorpoprotskiy.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //проверка работы методов(потом будет удалена)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+//                viewModel.deleteItemShopping(item)
+                viewModel.changeValueState(item)
+            }
         }
-        viewModel.getListShopping()
     }
 }
